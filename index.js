368eb0b7f1bfd04c011f69af004e595402947f8d
unknown <maryamihsan601@gmail.com>
2024-02-04 18:36:25 +0300

    first javascript home work

---
+++ index.js
@@ -0,0 +1,9 @@
+let name = 'maryam'
+let age = 29
+let isStudent = true ;
+console.log('hello my name is ' + name);
+console.log('my age is ' + age);
+console.log('Iam student');
+console.log('type of name var ' + typeof(name))
+console.log('type of age var ' + typeof age)
+console.log('type of isStudent var '+typeof isStudent)
